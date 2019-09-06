a41c454bfa4036799ba750194a2e39811743c9b5
ymanabe <ymanabeapp@gmail.com>
2019-09-07 04:01:44 +0900

    タスク作成機能を追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Folder;
 use App\Task;
+use App\Folder;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -14,13 +15,34 @@ class TaskController extends Controller
 
         $current_folder = Folder::find($id);
 
-        // $tasks = Task::where('folder_id', $current_folder->id)->get();
         $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
             'tasks' => $tasks,
+        ]);
+    }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id,
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
